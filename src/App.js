81ed62070cb55e4705d41f770e78c29315037a86
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-04-01 17:18:33 +0900

    useEffect(func, [state]) concept

--- src/App.js
+++ src/App.js
@@ -2,11 +2,38 @@ import Button from "./Button.js";
 // .module.css 파일 안 class 이름을 className으로 사용
 import styles from "./App.module.css";
 
+import {useState, useEffect} from "react";
+
+// useEffect(한번만 실행하고 싶은 함수 , [변하는 부분 알려주기])
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter change");
+  }, [keyword, counter]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
