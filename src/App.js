f9c8dc07f7e8596fe06f8690ea18621888075ab4
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-04-03 16:50:21 +0900

    example two : coin list

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    // ...array : array안의 itenm들을 만 return
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  // useEffect : [] 일 때는 1회만 실행
+  useEffect(() => {
+    // coin API 불러오기 (fetch, then)
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {/* array.map() : array안 item, index 를 순서대로 return */}
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {/* strong element는 해당 콘텐츠의 중요성이나 심각함, 긴급함 등을 강조할 때 사용합니다 */}
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {/* API를 불러와 coins state에서 map을 통해 coin 하나씩 정보를 가져옴 */}
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
