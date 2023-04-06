240a210df81755621a03b002967c35e37e302ab4
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-04-06 20:27:20 +0900

    Build movie web site 1 (async, await Fetch API , useEffect, map)

--- src/App.js
+++ src/App.js
@@ -2,34 +2,42 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  // useEffect : [] 일 때는 1회만 실행
-  useEffect(() => {
-    // coin API 불러오기 (fetch, then)
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {/* strong element는 해당 콘텐츠의 중요성이나 심각함, 긴급함 등을 강조할 때 사용합니다 */}
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {/* API를 불러와 coins state에서 map을 통해 coin 하나씩 정보를 가져옴 */}
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
-  );
+  const [movies, setMovies] = useState([]);
+  /* 기존 Fetch API 문법
+  fetch('http://example.com/movies.json')
+  .then((response) => response.json())
+  .then((data) => console.log(data)); 
+  */
+  // async, await 비동기식 Fetch API 문법
+  const getMovies = async () => {
+    const json = await(
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+        )
+      ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
+  // useEffect(()=>{}, []); 1회 동작
+  useEffect(() => {getMovies()}, []);
+
+  return <div>
+    {
+      loading ? <h1>Loading...</h1> 
+      : <div>
+          {movies.map((movie) => 
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image} />
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map((g) => (
+                <li key={g}>{g}</li>
+              ))}
+            </ul>
+          </div>)}
+        </div>
+    }
+  </div>
 }
 export default App;
