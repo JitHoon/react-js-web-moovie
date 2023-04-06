e8afa736bba781c05d484e59b27b0e6bbe6a87f5
JitHoon <101972330+JitHoon@users.noreply.github.com>
2023-04-06 21:39:32 +0900

    Build movie web site 4 (add  routes)

---
+++ src/routes/Home.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import Movies from "../components/Movies";
+
+export default function Home() {
+    const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  /* 기존 Fetch API 문법
+  fetch('http://example.com/movies.json')
+  .then((response) => response.json())
+  .then((data) => console.log(data)); 
+  */
+  // async, await 비동기식 Fetch API 문법
+  const getMovies = async () => {
+    const json = await(
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+        )
+      ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
+  // useEffect(()=>{}, []); 1회 동작
+  useEffect(() => {getMovies()}, []);
+
+  return <div>
+    {
+      loading ? (<h1>Loading...</h1>)
+      : (<div>
+          {movies.map((movie) => (
+          <Movies
+            key={movie.id} 
+            m_cover_img={movie.medium_cover_image} 
+            title={movie.title}
+            summary={movie.summary}
+            genres={movie.genres}
+          />
+          ))}
+        </div>)
+    }
+  </div>
+}
